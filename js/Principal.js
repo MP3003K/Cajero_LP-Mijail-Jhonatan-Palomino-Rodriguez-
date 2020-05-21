8302ebe51e87bf38cc5cc8ab88ea1620d12b3bfe
usuario169 <mijailpalomino@upeu.edu.pe>
2020-05-21 01:46:18 -0500

    maqueta_003

--- js/Principal.js
+++ js/Principal.js
@@ -25,5 +25,18 @@ function boton1(){
     if(n_vista==2){
         document.getElementById("m2").style.display="none";
         document.getElementById("m3").style.display="block";
+        n_vista++;
+    }
+    if(n_vista==3){
+        document.getElementById("m3").style.display="none";
+        document.getElementById("m4").style.display="block";
+        n_vista++;
+    }
+}
+function boton6(){
+    if(n_vista==4){
+        document.getElementById("m4").style.display="none";
+        document.getElementById("m5").style.display="block";
+        n_vista++;
     }
 }
