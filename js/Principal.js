68ba08b70800e12427e6fab256ae412999497531
usuario169 <mijailpalomino@upeu.edu.pe>
2020-05-21 01:07:12 -0500

    maquetado_002

--- js/Principal.js
+++ js/Principal.js
@@ -1,4 +1,5 @@
 var num = document.getElementById("datos");
+var n_vista= parseInt("1");
 function add(n){
    if(num.value<11111111){
  var x= num.value;
@@ -11,6 +12,7 @@ function login(){
     if(pass=="12345678"){
         document.getElementById("m1").style.display="none";
         document.getElementById("m2").style.display="block";
+    n_vista++;
     }else{
         document.getElementById("mensaje").innerHTML="Contraseña incorrecta......."
     }
@@ -18,4 +20,10 @@ function login(){
 function cancel(){
     num.value="";
     document.getElementById("mensaje").innerHTML="";
+}
+function boton1(){
+    if(n_vista==2){
+        document.getElementById("m2").style.display="none";
+        document.getElementById("m3").style.display="block";
+    }
 }
