34ccc3ae2c2bdeca716a0e54ea979eb1cff0fd43
usuario169 <mijailpalomino@upeu.edu.pe>
2020-05-20 23:57:38 -0500

    Maqueta_cajero

--- js/Principal.js
+++ js/Principal.js
@@ -1,3 +1,21 @@
-function adicionar(){
-alert("dfsf");
+var num = document.getElementById("datos");
+function add(n){
+   if(num.value<11111111){
+ var x= num.value;
+    num.value=x+n;
+
+   }
+}
+function login(){
+    var pass= document.getElementById("datos").value;
+    if(pass=="12345678"){
+        document.getElementById("m1").style.display="none";
+        document.getElementById("m2").style.display="block";
+    }else{
+        document.getElementById("mensaje").innerHTML="Contraseña incorrecta......."
+    }
+}
+function cancel(){
+    num.value="";
+    document.getElementById("mensaje").innerHTML="";
 }
