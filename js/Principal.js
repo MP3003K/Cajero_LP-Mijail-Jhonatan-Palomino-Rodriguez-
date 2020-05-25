b4a1b173cb74616cc3d4466088cc9587b2998fc5
usuario169 <mijailpalomino@upeu.edu.pe>
2020-05-25 12:26:48 -0500

    Prototipo

--- js/Principal.js
+++ js/Principal.js
@@ -1,21 +1,38 @@
 var num = document.getElementById("datos");
+var cantidad= document.getElementById("monto");
 var n_vista= parseInt("1");
 function add(n){
-   if(num.value<11111111){
- var x= num.value;
-    num.value=x+n;
+ 
+    if(n_vista==5){
+        if(cantidad.value<11111111){
+            if(cantidad.value<1){
+                cantidad.value="S/."
+            }
+            var y= cantidad.value;
+               cantidad.value=y+n;
+            }
+       }
 
+   if(n_vista==1){
+    if(num.value<11111111){
+        var x= num.value;
+           num.value=x+n;
+        }
    }
+  
+   
 }
 function login(){
     var pass= document.getElementById("datos").value;
-    if(pass=="12345678"){
+   if(n_vista==1){
+     if(pass=="12345678"){
         document.getElementById("m1").style.display="none";
         document.getElementById("m2").style.display="block";
     n_vista++;
     }else{
         document.getElementById("mensaje").innerHTML="Contraseña incorrecta......."
-    }
+    }  
+   }
 }
 function cancel(){
     num.value="";
@@ -39,4 +56,19 @@ function boton6(){
         document.getElementById("m5").style.display="block";
         n_vista++;
     }
+    if(n_vista==5){
+        document.getElementById("m5").style.display="none";
+        document.getElementById("m6").style.display="block";
+        n_vista++;
+    }
 }
+
+function boton8(){
+    if(n_vista==6){
+        document.getElementById("m6").style.display="none";
+        document.getElementById("m7").style.display="block";
+        n_vista++;
+    }
+    
+}
+
