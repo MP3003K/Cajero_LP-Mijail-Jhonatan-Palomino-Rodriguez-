c48a755759242f84e4c85a33ef02f4d041784328
usuario169 <mijailpalomino@upeu.edu.pe>
2020-05-25 14:41:49 -0500

    prototipo_f4

--- js/Principal.js
+++ js/Principal.js
@@ -2,17 +2,6 @@ var num = document.getElementById("datos");
 var cantidad= document.getElementById("monto");
 var n_vista= parseInt("1");
 function add(n){
- 
-    if(n_vista==5){
-        if(cantidad.value<11111111){
-            if(cantidad.value<1){
-                cantidad.value="S/."
-            }
-            var y= cantidad.value;
-               cantidad.value=y+n;
-            }
-       }
-
    if(n_vista==1){
     if(num.value<11111111){
         var x= num.value;
@@ -28,7 +17,7 @@ function login(){
      if(pass=="12345678"){
         document.getElementById("m1").style.display="none";
         document.getElementById("m2").style.display="block";
-    n_vista++;
+        n_vista++;
     }else{
         document.getElementById("mensaje").innerHTML="Contraseña incorrecta......."
     }  
@@ -37,32 +26,37 @@ function login(){
 function cancel(){
     num.value="";
     document.getElementById("mensaje").innerHTML="";
+    alert(n_vista);
 }
 function boton1(){
     if(n_vista==2){
         document.getElementById("m2").style.display="none";
         document.getElementById("m3").style.display="block";
         n_vista++;
-    }
-    if(n_vista==3){
-        document.getElementById("m3").style.display="none";
-        document.getElementById("m4").style.display="block";
-        n_vista++;
+    }else{
+        if(n_vista==3){
+            document.getElementById("m3").style.display="none";
+            document.getElementById("m4").style.display="block";
+            n_vista++;
+        }
     }
 }
 function boton6(){
+  
     if(n_vista==4){
         document.getElementById("m4").style.display="none";
         document.getElementById("m5").style.display="block";
         n_vista++;
     }
-    if(n_vista==5){
+}
+
+function boton4(){
+        if(n_vista==5){
         document.getElementById("m5").style.display="none";
         document.getElementById("m6").style.display="block";
         n_vista++;
-    }
+        }
 }
-
 function boton8(){
     if(n_vista==6){
         document.getElementById("m6").style.display="none";
